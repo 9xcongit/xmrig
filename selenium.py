1f89a663070a41f6bd24ec10862ff9e767ecdcdf
tieuvy9001 <tieuvy9001@gmail.com>
2025-03-26 14:19:44 +0700

    Create selenium.py

---
+++ selenium.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')  
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+
+driver1 = webdriver.Chrome(options=chrome_options)
+
+
+url1 = "https://9xhomestyle.blogspot.com"
+driver1.get(url1)
+print(f"opening: {url1}")
+
+
+time.sleep(60*60*60)
+time.sleep(60*60*60)
